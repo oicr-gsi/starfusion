772013dc5331b680c653909f182801afc7b8e078
alkabenawra <alkabenawra@rogers.com>
2018-06-15 09:13:15 -0400

    Added tabix program

--- workflow-starfusion/src/main/java/ca/on/oicr/pde/workflows/STARfusionWorkflowClient.java
+++ workflow-starfusion/src/main/java/ca/on/oicr/pde/workflows/STARfusionWorkflowClient.java
@@ -134,7 +134,7 @@ public class STARfusionWorkflowClient extends OicrWorkflow {
          * outputFile, model-fit.zip
          */
         // workflow : read inputs read1 fastq and read2 fastq file; run star-fusion; write the output to temp directory; 
-        // run sequenzaR; handle output; provision files (3) -- model-fit.zip; text/plain; text/plain
+        // run sequenzaR; handle output; provision files (3) -- .tsv, .tsv, .tsv;
         Job parentJob = null;
         this.outDir = this.outputFilenamePrefix + "_output";
 
@@ -170,6 +170,10 @@ public class STARfusionWorkflowClient extends OicrWorkflow {
         cmd.addArgument("export PATH=/oicr/local/analysis/sw/star/STAR-2.6.0c/bin/Linux_x86_64_static:$PATH"+ ";");
         cmd.addArgument("export PATH=/oicr/local/analysis/sw//samtools/samtools-0.1.17:$PATH"+ ";");
         cmd.addArgument("export PATH=/oicr/local/analysis/sw/starfusion/STAR-Fusion-v1.4.0:$PATH"+ ";");
+        cmd.addArgument("export LD_LIBRARY_PATH=/oicr/local/analysis/sw/tabix/tabix-0.2.6:$LD_LIBRARY_PATH");
+        cmd.addArgument("export PATH=/oicr/local/analysis/sw/tabix/tabix-0.2.6:$PATH");
+        cmd.addArgument("export PERL5LIB=/oicr/local/analysis/sw/tabix/tabix-0.2.6/lib/perl5:$PERL5LIB");
+        cmd.addArgument("export TABIXROOT=/oicr/local/analysis/sw/tabix/tabix-0.2.6");
         cmd.addArgument("STAR-Fusion");
         cmd.addArgument("--genome_lib_dir " + this.refGenome);
         cmd.addArgument("--left_fq " + this.read1Fastq);
